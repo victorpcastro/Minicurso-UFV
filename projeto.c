b6777ffe476c2db904a00672b22d9dc927601782
Victor <viorpaiva08@hotmail.com>
2019-06-13 20:27:08 -0300

    adicionando funcao de multiplicacao

---
+++ projeto.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int soma(int a, int b){
+	int c;
+	c=a+b;
+	return c;
+}
+int subtracao(int a, int b){
+	int c;
+	c=a-b;
+	return c;
+}
+int multiplicacao(int a, int b){
+	int c;
+	c=a*b;
+	return c;
+}
+
+int main(){
+	
+	int a,b,c,d;
+	c= soma(5,2);
+	printf("a soma eh: %d\n",c);
+	d=subtracao(5,2);
+	printf("%d\n",d);
+	a=multiplicacao(5,2);
+	printf("%d\n",a);
+	return 0;
+}
